46ea8b2f05dd54666b235189be606877f37e2b32
Jennifer Cobb <jennifercobb829@gmail.com>
2026-05-31 09:15:21 -0400

    Update

--- netflix-clone/src/components/TitleCards/TitleCards.jsx
+++ netflix-clone/src/components/TitleCards/TitleCards.jsx
@@ -1,15 +1,33 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTRkZTk2ZGM2ZGQwNmVhNDU1Y2YyOTVjZTYzZDhmOCIsIm5iZiI6MTc4MDIzMTc3MC41Miwic3ViIjoiNmExYzJlNWE0OGM0MTRmNjk5MWUxOWY5Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.n8frSbaNOoC9MFjcg3IbVn2CiJFwF2yVygb5_p5wkNI",
+    },
+  };
 
   const handleWheel = (event) => {
     event.preventDefault();
     cardsRef.current.scrolLeft += event.deltaY;
   };
   useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      options,
+    )
+      .then((repsonse) => response.json())
+      .then((response) => setApiData(response.results))
+      .catch((err) => console.error(err));
+
     cardsRef.current.addEventListener("wheel", handleWheel);
   }, []);
 
@@ -17,11 +35,14 @@ const TitleCards = ({ title, category }) => {
     <div className="title-cards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
